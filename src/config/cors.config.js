9174d8b60b5bc4c1261442e6aaf526c0440d2c9a
toonajuig <thirawat.devtt@gmail.com>
2026-06-02 17:55:42 +0700

    fix: add sprint2 frontend URL to CORS whitelist

--- src/config/cors.config.js
+++ src/config/cors.config.js
@@ -4,6 +4,7 @@ export const corsOptions = {
     "http://localhost:5174",
     "http://localhost:5175",
     "https://ecommerce-frontend-sprint3.vercel.app",
+    "https://group5-ecommerce-frontend-sprint2.vercel.app",
   ], //This are frontend domain.
   credentials: true, //Allow cookies to be send
 };
