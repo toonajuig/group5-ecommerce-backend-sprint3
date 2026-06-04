d57f0a74e5997cd5a04bf857e6928b29776a0b4a
toonajuig <thirawat.devtt@gmail.com>
2026-06-04 16:32:52 +0700

    feat: add GET /admin/products endpoint returning all products including inactive

--- src/modules/admin/admin.controller.js
+++ src/modules/admin/admin.controller.js
@@ -1,6 +1,15 @@
 import { Product } from "../products/product.model.js";
 import { User } from "../user/user.model.js";
 
+export const getAdminProducts = async (req, res, next) => {
+    try {
+        const products = await Product.find({}).sort({ createdAt: -1 });
+        return res.status(200).json({ success: true, count: products.length, data: products });
+    } catch (error) {
+        next(error);
+    }
+};
+
 export const getAdminStats = async (req, res, next) => {
     try {
         const [totalProducts, totalCustomers] = await Promise.all([
