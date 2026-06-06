928be0da2fbf70497b19b0145a6e1b8704f53bef
toonajuig <thirawat.devtt@gmail.com>
2026-06-07 01:20:02 +0700

    feat: add profile update and password change endpoints for users

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -17,6 +17,8 @@ import {
   addUserAddress,
   updateUserAddress,
   deleteUserAddress,
+  updateMyProfile,
+  changeMyPassword,
 } from "../modules/user/user.controller.js";
 
 router.post("/login", userLogin);
@@ -27,6 +29,8 @@ router.post("/me/addresses", authUser, addUserAddress);
 router.put("/me/addresses/:addressId", authUser, updateUserAddress);
 router.delete("/me/addresses/:addressId", authUser, deleteUserAddress);
 router.post("/", createUser);
+router.put("/me", authUser, updateMyProfile);
+router.put("/me/password", authUser, changeMyPassword);
 router.get("/", authUser, adminOnly, getUsers);
 router.get("/:id", authUser, adminOnly, getUser);
 router.put("/:id", authUser, adminOnly, updateUser);
